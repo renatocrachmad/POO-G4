07c7cb176f74122d0d09979a7ff599baf5db0c35
MatheusHenrichs <henrichsmatheus@gmail.com>
2024-04-12 19:36:46 -0300

    feat: criaçao dos atributos nas entidades

--- projeto/src/br/com/poo/g4/entities/ContaCorrente.java
+++ projeto/src/br/com/poo/g4/entities/ContaCorrente.java
@@ -1,5 +1,28 @@
 package br.com.poo.g4.entities;
 
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
 
+	Double taxa;
+
+	public ContaCorrente() {
+		super();
+	}
+
+	public ContaCorrente(String cpf, double saldo, String agencia, String tipo, Double rendimento, Double taxa) {
+		super(cpf, saldo, agencia, tipo, rendimento);
+		this.taxa = taxa;
+	}
+
+	public Double getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(Double taxa) {
+		this.taxa = taxa;
+	}
+
+	@Override
+	public String toString() {
+		return "ContaCorrente [taxa=" + taxa + "]";
+	}				
 }
--- projeto/src/br/com/poo/g4/entities/ContaPoupanca.java
+++ projeto/src/br/com/poo/g4/entities/ContaPoupanca.java
@@ -1,5 +1,29 @@
 package br.com.poo.g4.entities;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta {
 
+	Double rendimento;
+	
+	
+
+	public ContaPoupanca() {
+		super();
+			}
+
+	public ContaPoupanca(String cpf, double saldo, String agencia, String tipo, Double rendimento) {
+		super(cpf, saldo, agencia, tipo, rendimento);		
+	}
+	
+	public Double getRendimento() {
+		return rendimento;
+	}
+
+	public void setRendimento(Double rendimento) {
+		this.rendimento = rendimento;
+	}
+
+	@Override
+	public String toString() {
+		return "ContaPoupanca [rendimento=" + rendimento + "]";
+	}				
 }
