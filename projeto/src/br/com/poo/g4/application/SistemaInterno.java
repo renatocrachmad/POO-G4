ca294c90f24e98bc48d5dcf39b2497d1b3513f52
MatheusHenrichs <henrichsmatheus@gmail.com>
2024-04-12 19:45:52 -0300

    feat :adiçao dos testes dos metodos de(sacar,depositar,trans)

--- projeto/src/br/com/poo/g4/application/SistemaInterno.java
+++ projeto/src/br/com/poo/g4/application/SistemaInterno.java
@@ -1,10 +1,18 @@
 package br.com.poo.g4.application;
 
+import br.com.poo.g4.entities.ContaCorrente;
+
 public class SistemaInterno {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ContaCorrente contaCorrente = new ContaCorrente("123.456.789-11",500.00,"011","Conta corrente",0.10,0.10);
+		//metodo depositar
+		//contaCorrente.depositar(-400.00);
+		//metodo sacar
+		//contaCorrente.sacar(300.00);
+		//metodo transferir
+		contaCorrente.transferir(contaCorrente, 200);
 	}
 
 }
