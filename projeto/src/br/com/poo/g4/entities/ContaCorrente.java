2090444fe13b3419fe2242ae1d514f4fc3831a35
renatocrachmad <162373494+renatocrachmad@users.noreply.github.com>
2024-04-12 19:44:18 -0300

    Merge branch 'development' of https://github.com/renatocrachmad/POO-G4.git into development

--- projeto/src/br/com/poo/g4/entities/ContaCorrente.java
+++ projeto/src/br/com/poo/g4/entities/ContaCorrente.java
@@ -1,5 +1,12 @@
 package br.com.poo.g4.entities;
 
 public class ContaCorrente {
+	private String relatorioTributa;
+	private double saldo;
+	private String agencia;
+	private String tipo;
+	private double deposito;
+	private double transferencia;
+	private double saque;
 
 }
