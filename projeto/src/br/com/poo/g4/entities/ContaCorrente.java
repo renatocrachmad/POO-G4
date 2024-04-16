440115ee9814f12e1e6f5dabdbbf6a4184389c0c
MatheusHenrichs <henrichsmatheus@gmail.com>
2024-04-16 20:43:04 -0300

    feat : adiçao das taxas nos metodos

--- projeto/src/br/com/poo/g4/entities/ContaCorrente.java
+++ projeto/src/br/com/poo/g4/entities/ContaCorrente.java
@@ -1,26 +1,53 @@
 package br.com.poo.g4.entities;
+
 public class ContaCorrente extends Conta {
-	Double taxa;
-	
+	private double saldo;
+	private double totalGasto;	
+	private double taxa;
+
 	public ContaCorrente() {
 		super();
-	}		
-	
+	}
+
 	public ContaCorrente(String cpf, String agencia, Double taxa) {
 		super(cpf, agencia, "CORRENTE");
 		this.taxa = taxa;
 	}
 
-	public Double getTaxa() {
-		return taxa;
-	}
-
-	public void setTaxa(Double taxa) {
-		this.taxa = taxa;
+	@Override
+	public void sacar(double valor) {
+		this.taxa = 0.10;
+		if (saldo >= valor + taxa) {
+			saldo -= valor + taxa;
+			totalGasto += taxa;
+		} else {
+			System.out.println("Saldo insuficiente para saque.");
+		}
 	}
 
 	@Override
-	public String toString() {
-		return "ContaCorrente [taxa=" + taxa + "]";
-	}				
+	public void depositar(double valor) {
+		this.taxa = 0.10;
+		saldo += valor - taxa;
+		totalGasto += taxa;
+	}
+
+	@Override
+	public void transferir(Conta contaDestino, double valor) {
+		this.taxa = 0.20;
+		if (saldo >= valor + taxa) {
+			saldo -= valor + taxa;
+			contaDestino.depositar(valor);
+			totalGasto += taxa;
+		} else {
+			System.out.println("Saldo insuficiente para transferência.");
+		}
+	}
+
+	public void relatorioTributacao() {
+		System.out.println("Total gasto em operações: R$" + totalGasto);
+		System.out.println("Taxa por saque: R$" + taxa);
+		System.out.println("Taxa por depósito: R$" + taxa);
+		System.out.println("Taxa por transferência: R$" + taxa);
+	}
 }
